df99ca67ae63ce0df5e43db236e26650ee57bbca
gab2112 <gabriel.vgonzalez@alumnos.udg.mx>
2022-03-20 18:42:41 -0600

    Actualizada documentacion

--- web/js/scripts2.js
+++ web/js/scripts2.js
@@ -21,3 +21,29 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Modal Exito
+const modAlerta = document.getElementById("modAlerta");
+const enviarBtn = document.getElementById("ejemploM");
+enviarBtn.addEventListener("click", mostrar);
+const alertClose = document.getElementById("alertClose");
+alertClose.addEventListener("click", ocultar);
+function ocultar(){
+  modAlerta.classList.add("oculto");
+}
+function mostrar(){
+  modAlerta.classList.remove("oculto");
+}
+
+// Modal Error
+const modAlerta2 = document.getElementById("modAlerta2");
+const enviarBtn2 = document.getElementById("ejemploM2");
+enviarBtn2.addEventListener("click", mostrar2);
+const alertClose2 = document.getElementById("alertClose2");
+alertClose2.addEventListener("click", ocultar2);
+function ocultar2(){
+  modAlerta2.classList.add("oculto");
+}
+function mostrar2(){
+  modAlerta2.classList.remove("oculto");
+}
